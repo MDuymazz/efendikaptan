1dc379183e378507fee46f3063c0f217a8917708
MDuymazz <duymaz.mrt@gmail.com>
2025-03-21 12:00:11 +0300

    Update data_html.py

--- data_html.py
+++ data_html.py
@@ -2,11 +2,19 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
+
+# Chrome seçeneklerini ayarlıyoruz
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Başsız modda çalıştırma (isteğe bağlı)
+chrome_options.add_argument("--no-sandbox")  # Bazı sistemlerde sandbox modunun kapatılması gerekebilir
+chrome_options.add_argument("--disable-dev-shm-usage")  # Dev/shm kullanımını devre dışı bırakma
+chrome_options.add_argument(f"user-data-dir=/tmp/chrome-user-data-dir")  # Geçici bir user data dizini kullanıyoruz
 
 # ChromeDriver'ı yükleyip başlatmak için Service kullanma
 service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 driver.get('https://proxyium.com/')
 
